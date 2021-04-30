87ca9c6de7987e8d980f9ec99b4f1168935f2a53
Sandip Swain <swainsandip9@gmail.com>
2021-04-30 14:01:17 +0530

    Done till Deletion of Data

--- mongodb.js
+++ mongodb.js
@@ -15,23 +15,10 @@ MongoClient.connect(
     // Its takes in a database name and gives back a database reference and connects to a specific database
     const db = client.db(databaseName);
 
-    // Updating Documents
-    // Update One
     // db.collection("users")
-    //   .updateOne(
-    //     {
-    //       _id: new ObjectID("608af3e473a1332bbc79b0c5"),
-    //     },
-    //     {
-    //       // $set: {
-    //       //   name: "Mike",
-    //       // },
-
-    //       $inc: {
-    //         age: 3,
-    //       },
-    //     }
-    //   )
+    //   .deleteMany({
+    //     age: 27,
+    //   })
     //   .then((result) => {
     //     console.log(result);
     //   })
@@ -39,18 +26,10 @@ MongoClient.connect(
     //     console.log(error);
     //   });
 
-    // Update Many
     db.collection("check")
-      .updateMany(
-        {
-          complted: false,
-        },
-        {
-          $set: {
-            complted: true,
-          },
-        }
-      )
+      .deleteOne({
+        description: "Hey there",
+      })
       .then((result) => {
         console.log(result);
       })
