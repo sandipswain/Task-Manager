abc29fb16d991d9a294d672e168d0e66bd018bb6
Sandip Swain <swainsandip9@gmail.com>
2021-05-04 00:33:13 +0530

    Modified the mongoose required message

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useNewUrlParser: true,
   useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 // 1st para: Name of the model , 2nd para: The argument to pass
