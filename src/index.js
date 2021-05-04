542b16918910607110d2bb05c8b5e588c72f768b
Sandip Swain <swainsandip9@gmail.com>
2021-05-04 14:41:17 +0530

    Converted the express handles with Async/Await Syntax

--- src/index.js
+++ src/index.js
@@ -12,83 +12,124 @@ app.use(express.json());
 // RESTAPI route
 
 // Users
-app.post("/users", (req, res) => {
+app.post("/users", async (req, res) => {
   const user = new User(req.body);
 
-  user
-    .save()
-    .then(() => {
-      res.status(201).send(user);
-    })
-    .catch((error) => {
-      res.status(400).send(error);
-    });
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+
+  // user
+  //   .save()
+  //   .then(() => {
+  //     res.status(201).send(user);
+  //   })
+  //   .catch((error) => {
+  //     res.status(400).send(error);
+  //   });
 });
 
 // Sending Get Request to fetch all the users
-app.get("/users", (req, res) => {
-  User.find({})
-    .then((users) => {
-      res.send(users);
-    })
-    .catch((error) => {});
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+
+  // User.find({})
+  //   .then((users) => {
+  //     res.send(users);
+  //   })
+  //   .catch((error) => {});
 });
 
 // Sending request to fetch a user dynamically using IDS
-app.get("/users/:id", (req, res) => {
+app.get("/users/:id", async (req, res) => {
   const _id = req.params.id;
+
+  try {
+    const user = await User.findById(_id);
+    if (!user) return res.status(404).send();
+    res.send(user);
+  } catch (error) {
+    res.status(500).send();
+  }
   // Param contents the id value of the URL
-  User.findById(_id)
-    .then((user) => {
-      if (!user) {
-        return res.status(404).send();
-      }
-      res.send(user);
-    })
-    .catch((error) => {
-      res.status(500).send();
-    });
+  // User.findById(_id)
+  //   .then((user) => {
+  //     if (!user) {
+  //       return res.status(404).send();
+  //     }
+  //     res.send(user);
+  //   })
+  //   .catch((error) => {
+  //     res.status(500).send();
+  //   });
 });
 
 // Tasks
 
-app.post("/tasks", (req, res) => {
+app.post("/tasks", async (req, res) => {
   const task = new Task(req.body);
+  try {
+    await task.save();
+    res.status(201).send(task);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 
-  task
-    .save()
-    .then(() => {
-      res.status(201).send(task);
-    })
-    .catch((error) => {
-      res.status(400).send(error);
-    });
+  // task
+  //   .save()
+  //   .then(() => {
+  //     res.status(201).send(task);
+  //   })
+  //   .catch((error) => {
+  //     res.status(400).send(error);
+  //   });
+});
+
+// Sending a request to fetch all the tasks
+app.get("/tasks", async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.send(tasks);
+  } catch (error) {
+    res.status(500).send();
+  }
+  // Task.find({})
+  //   .then((tasks) => {
+  //     res.send(tasks);
+  //   })
+  //   .catch((error) => {
+  //     res.status(500).send();
+  //   });
+});
+
+// Sending request to dynamically fetch a single task
+app.get("/tasks/:id", async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const task = await Task.findById(_id);
+    if (!task) return res.status(400).send();
+    res.send(task);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+  // Task.findById(_id)
+  //   .then((task) => {
+  //     if (!task) return res.status(404).send();
+  //     res.send(task);
+  //   })
+  //   .catch((error) => {
+  //     res.status(500).send(error);
+  //   });
 });
 
 app.listen(port, () => {
   console.log("Server is up on port " + port);
-});
-
-// Sending a request to fetch all the tasks
-app.get("/tasks", (req, res) => {
-  Task.find({})
-    .then((tasks) => {
-      res.send(tasks);
-    })
-    .catch((error) => {
-      res.status(500).send();
-    });
-});
-
-// Sending request to dynamically fetch a single task
-app.get("/tasks/:id", (req, res) => {
-  const _id = req.params.id;
-  Task.findById(_id)
-    .then((task) => {
-      if (!task) return res.status(404).send();
-      res.send(task);
-    })
-    .catch((error) => {
-      res.status(500).send(error);
-    });
 });
