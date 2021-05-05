96511d2affa6f46b28e5872c75429ec4d785f743
Sandip Swain <swainsandip9@gmail.com>
2021-05-05 19:14:47 +0530

    Made seperate Router files for User and Task

--- src/index.js
+++ src/index.js
@@ -1,159 +1,21 @@
 const express = require("express");
 require("./db/mongoose");
-const User = require("./models/user");
-const Task = require("./models/task");
+const userRouter = require("./routers/user");
+const taskRouter = require("./routers/task");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // It automatically converts incoming JSON to Objects
 app.use(express.json());
+app.use(userRouter);
+app.use(taskRouter);
 
 // RESTAPI route
 
-//*********** **************/ Users\****************************\\
-app.post("/users", async (req, res) => {
-  const user = new User(req.body);
-
-  try {
-    await user.save();
-    res.status(201).send(user);
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
-
-// Sending Get Request to fetch all the users
-app.get("/users", async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-// Sending request to fetch a user dynamically using IDS
-app.get("/users/:id", async (req, res) => {
-  const _id = req.params.id;
-
-  try {
-    const user = await User.findById(_id);
-    if (!user) return res.status(404).send();
-    res.send(user);
-  } catch (error) {
-    res.status(500).send();
-  }
-});
-
-// Resource Updating Endpoints
-app.patch("/users/:id", async (req, res) => {
-  const updates = Object.keys(req.body);
-  const allowedUpdates = ["name", "email", "password", "age"];
-  const isValidOperation = updates.every((update) =>
-    allowedUpdates.includes(update)
-  );
-
-  if (!isValidOperation) {
-    return res.status(400).send({ error: "Invalid Updates!" });
-  }
-
-  try {
-    // This will return the new user as opposed to the existing one that was found before the update
-    // That is we will get the original user with the updates applied
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-
-    if (!user) {
-      return res.status(404).send();
-    }
-
-    res.send(user);
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
-
-// Resource Deleting Endpoints
-app.delete("/users/:id", async (req, res) => {
-  try {
-    const user = await User.findByIdAndDelete(req.params.id);
-    if (!user) return res.status(404).send();
-    res.send(user);
-  } catch (error) {
-    res.status(500).send();
-  }
-});
+//*************************/ Users \****************************\\
 
 // **************************Tasks**************************\\
-
-app.post("/tasks", async (req, res) => {
-  const task = new Task(req.body);
-  try {
-    await task.save();
-    res.status(201).send(task);
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
-
-// Sending a request to fetch all the tasks
-app.get("/tasks", async (req, res) => {
-  try {
-    const tasks = await Task.find({});
-    res.send(tasks);
-  } catch (error) {
-    res.status(500).send();
-  }
-});
-
-// Sending request to dynamically fetch a single task
-app.get("/tasks/:id", async (req, res) => {
-  const _id = req.params.id;
-  try {
-    const task = await Task.findById(_id);
-    if (!task) return res.status(400).send();
-    res.send(task);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-// Updating Documents
-
-app.patch("/tasks/:id", async (req, res) => {
-  const updates = Object.keys(req.body);
-  const allowedUpdates = ["description", "completed"];
-  const isValidOperation = updates.every((update) =>
-    allowedUpdates.includes(update)
-  );
-
-  if (!isValidOperation)
-    return res.status(400).send({ error: "Invalid Updates!" });
-  try {
-    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-
-    if (!task) return res.status(404).send();
-    res.send(task);
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
-
-app.delete("/tasks/:id", async (req, res) => {
-  try {
-    const task = await Task.findByIdAndDelete(req.params.id);
-    if (!task) return res.status(404).send();
-    res.send(task);
-  } catch (error) {
-    res.status(500).send();
-  }
-});
 
 app.listen(port, () => {
   console.log("Server is up on port " + port);
---
+++ src/routers/user.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const User = require("../models/user");
+const router = new express.Router();
+
+router.post("/users", async (req, res) => {
+  const user = new User(req.body);
+
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+// Sending Get Request to fetch all the users
+router.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// Sending request to fetch a user dynamically using IDS
+router.get("/users/:id", async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const user = await User.findById(_id);
+    if (!user) return res.status(404).send();
+    res.send(user);
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+// Resource Updating Endpoints
+router.patch("/users/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["name", "email", "password", "age"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid Updates!" });
+  }
+
+  try {
+    // This will return the new user as opposed to the existing one that was found before the update
+    // That is we will get the original user with the updates applied
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!user) {
+      return res.status(404).send();
+    }
+
+    res.send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+// Resource Deleting Endpoints
+router.delete("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) return res.status(404).send();
+    res.send(user);
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+module.exports = router;
