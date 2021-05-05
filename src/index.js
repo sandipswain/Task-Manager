f12fe93debd893d2c9c6604a5a300b537b668409
Sandip Swain <swainsandip9@gmail.com>
2021-05-05 11:24:39 +0530

    Added Deleted Contents Endpoints

--- src/index.js
+++ src/index.js
@@ -76,6 +76,17 @@ app.patch("/users/:id", async (req, res) => {
   }
 });
 
+// Resource Deleting Endpoints
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) return res.status(404).send();
+    res.send(user);
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
 // **************************Tasks**************************\\
 
 app.post("/tasks", async (req, res) => {
@@ -131,6 +142,16 @@ app.patch("/tasks/:id", async (req, res) => {
     res.send(task);
   } catch (error) {
     res.status(400).send(error);
+  }
+});
+
+app.delete("/tasks/:id", async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if (!task) return res.status(404).send();
+    res.send(task);
+  } catch (error) {
+    res.status(500).send();
   }
 });
 
