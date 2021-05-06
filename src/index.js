26e49d697f5ae758af9eb4adb94dc973cff01504
Sandip Swain <swainsandip9@gmail.com>
2021-05-06 09:49:02 +0530

    Understood bcrypt with an example

--- src/index.js
+++ src/index.js
@@ -10,13 +10,20 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
-
-// RESTAPI route
-
-//*************************/ Users \****************************\\
-
-// **************************Tasks**************************\\
-
 app.listen(port, () => {
   console.log("Server is up on port " + port);
 });
+
+const bcrypt = require("bcryptjs");
+const myFunction = async () => {
+  const password = "Red12345!";
+  const hashedPassword = await bcrypt.hash(password, 8);
+
+  console.log(password);
+  console.log(hashedPassword);
+
+  const isMatch = await bcrypt.compare("red12345!", hashedPassword);
+  console.log(isMatch);
+};
+
+myFunction();
