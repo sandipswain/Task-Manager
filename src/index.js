fe45f57d63a14e62b12f70f8c2291429d0b83ae6
Sandip Swain <swainsandip9@gmail.com>
2021-05-07 23:28:45 +0530

    Learnt about JWT tokens

--- src/index.js
+++ src/index.js
@@ -14,6 +14,22 @@ app.listen(port, () => {
   console.log("Server is up on port " + port);
 });
 
+// Create a new authentication token
+const jwt = require("jsonwebtoken");
+const myFunction = async () => {
+  // Created a JWT token with id
+  // param(uniqueid,secretmessage,timeofexpire)
+  const token = jwt.sign({ _id: "abc123" }, "thisisme", {
+    expiresIn: "7 days",
+  });
+  console.log(token);
+
+  // Verify the JWT token
+  const data = jwt.verify(token, "thisisme");
+  console.log(data);
+};
+myFunction();
+
 // Example of hashed password
 
 // const bcrypt = require("bcryptjs");
