3c717e8a3dc1eb858a22b82a721f67379e71d2c6
Sandip Swain <swainsandip9@gmail.com>
2021-05-15 12:29:09 +0530

    Learnt about uploading files according to filetype using Regex

--- src/index.js
+++ src/index.js
@@ -13,6 +13,21 @@ const upload = multer({
   limits: {
     fileSize: 1000000,
   },
+  // req->Request being made , file -> request about the file being uploaded , cb -> tells us when we are done uploading a file
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(".(doc|docx)$")) {
+      return cb(new Error("Please upload a Word Docx"));
+    }
+    cb(undefined, true);
+    // if something goes wrong
+    // cb(new Error("File must be a PDF"));
+    // // If all goes well
+    // // undefined as 1st here states that everything went well
+    // // true states that file is ready to be uploaded
+    // cb(undefined, true);
+    // // false states the file is rejected
+    // cb(undefined, false);
+  },
 });
 
 // A Endpoint where client will be able to upload files
